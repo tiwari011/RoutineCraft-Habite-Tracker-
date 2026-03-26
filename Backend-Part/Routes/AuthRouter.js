9ab02f715197d3dec0d8a2710f60ccb051e7b724
Satyam Tiwari <satyamtiwari454obra@gmail.com>
2026-03-26 23:50:09 +0530

    Removed .env from tracking

--- Backend-Part/Routes/AuthRouter.js
+++ Backend-Part/Routes/AuthRouter.js
@@ -1,15 +1,44 @@
-const { Signup , login, googleLogin } = require('../Controllers/AuthController');
-const { SignupValidation , loginValidation} = require('../Middlewares/AuthValidation');
-const router = require ('express').Router();
-// router.post('/login',(req, res)=>{
-    //     res.send('login successful')
-    // })
-    // router.post('/signup',(req, res)=>{
-        //     res.send('registered successful')
-        // })
-        router.post('/login',loginValidation, login)
-        router.post('/signup',SignupValidation, Signup)
-//   GOOGLE LOGIN ROUTE 
-router.post('/google', googleLogin);
+// const { Signup , login, googleLogin } = require('../Controllers/AuthController');
+// const { SignupValidation , loginValidation} = require('../Middlewares/AuthValidation');
+// const router = require ('express').Router();
+// // router.post('/login',(req, res)=>{
+//     //     res.send('login successful')
+//     // })
+//     // router.post('/signup',(req, res)=>{
+//         //     res.send('registered successful')
+//         // })
+//         router.post('/login',loginValidation, login)
+//         router.post('/signup',SignupValidation, Signup)
+// //   GOOGLE LOGIN ROUTE 
+// router.post('/google', googleLogin);
 
-module.exports=router;
+// module.exports=router;
+const { Signup, login, googleLogin } = require('../Controllers/AuthController');
+const { SignupValidation, loginValidation } = require('../Middlewares/AuthValidation');
+const router = require('express').Router();
+
+//  Normal auth routes
+router.post('/login', loginValidation, login);
+router.post('/signup', SignupValidation, Signup);
+
+// GOOGLE LOGIN
+
+//  Start Google login 
+router.get('/google', googleLogin);
+
+//  Google callback 
+router.get('/google/callback', async (req, res) => {
+  try {
+    const code = req.query.code;
+
+    // call controller
+   
+    res.send("Google login successful ✅");
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Google login failed");
+  }
+});
+
+module.exports = router;
